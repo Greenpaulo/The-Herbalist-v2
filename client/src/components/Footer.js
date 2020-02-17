30e12e479f7367671234fadc3500ec94080dc2d9
Paul Bingham <greenpaulo@yahoo.co.uk>
2020-02-17 09:18:05 +0000

    Changed footer to dynamic date

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Footer extends Component {
+
+  getYear () {
+    const year = new Date().getFullYear();
+    return year;
+  }
+
+
   render() {
     return (
       <footer className="bg-dark">
         <div>
           <Link to="/" className="logo">The Herbalist</Link>
-          <span>&copy; 2019</span>
+          <span>&copy; {this.getYear()}</span>
         </div>
         <ul>
           <li><Link to="/terms">Terms and Conditions</Link></li>
